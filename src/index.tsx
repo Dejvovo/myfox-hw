963660778cdc01d54023b01539a6bad26fdf8e7e
David Goliard <david.goliard@beafuture.com>
2026-02-01 00:43:21 +0100

    Layout added

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { MyReservationPage } from "./Pages/MyReservationsPage";
+import { Header } from "./Pages/Header";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -8,6 +9,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
+    <Header></Header>
     <MyReservationPage />
   </React.StrictMode>
 );
